26274bdbc2f0348798ec0c7a760de8eec5e3a9a7
Senul <hardlove.mrplus@gmail.com>
2023-08-16 14:21:20 +0530

    ipfs route added

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,7 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { authRouter } from "./routers/auth";
 import { chaptersRouter } from "./routers/chapters";
+import { ipfsRouter } from "./routers/ipfs";
 
 /**
  * This is the primary router for your server.
@@ -10,6 +11,7 @@ import { chaptersRouter } from "./routers/chapters";
 export const appRouter = createTRPCRouter({
   auth: authRouter,
   chapters: chaptersRouter,
+  ipfs: ipfsRouter,
 });
 
 // export type definition of API
