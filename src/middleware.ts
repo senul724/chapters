62858aa789f5d2b708c0d4f791b010dd3fd3880b
Senul <hardlove.mrplus@gmail.com>
2023-08-16 14:22:05 +0530

    middleware fixed

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,9 @@
+import { jwtVerify } from "jose";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { jwt_key } from "./utils/phrase_formatter";
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
 
   const session = request.cookies.get("_session")?.value;
@@ -14,9 +16,16 @@ export function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL("/story", request.url));
   }
 
+  if (path === "/activities" && session) {
+    const { payload } = await jwtVerify(session, jwt_key);
+    const { address } = payload as { address: string };
+
+    return NextResponse.rewrite(new URL(`/activities/${address}`, request.url));
+  }
+
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/", "/story", "/dashboard", "/complete"],
+  matcher: ["/", "/story", "/complete", "/activities"],
 };
