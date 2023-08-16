1161f67b66d8a98c86563a88fb59f7f528b054a7
Senul <hardlove.mrplus@gmail.com>
2023-08-16 14:17:15 +0530

    new env variable addded

--- src/env.mjs
+++ src/env.mjs
@@ -9,6 +9,10 @@ export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
     NODE_ENV: z.enum(["development", "test", "production"]),
+    INFURA_PROJECT_ID:z.string(),
+    INFURA_PROJECT_SECRET:z.string(),
+    JWT_SECRET:z.string(),
+    NFT_IMAGE:z.string(),
   },
 
   /**
@@ -17,7 +21,8 @@ export const env = createEnv({
    * `NEXT_PUBLIC_`.
    */
   client: {
-    // NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
+    NEXT_PUBLIC_FORTMATIC_MAIN:z.string(),
+    NEXT_PUBLIC_FORTMATIC_TEST:z.string(),
   },
 
   /**
@@ -27,7 +32,12 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
-    // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+    INFURA_PROJECT_ID: process.env.INFURA_PROJECT_ID,
+    INFURA_PROJECT_SECRET: process.env.INFURA_PROJECT_SECRET,
+    NEXT_PUBLIC_FORTMATIC_TEST:process.env.NEXT_PUBLIC_FORTMATIC_TEST,
+    NEXT_PUBLIC_FORTMATIC_MAIN:process.env.NEXT_PUBLIC_FORTMATIC_MAIN,
+    JWT_SECRET:process.env.JWT_SECRET,
+    NFT_IMAGE:process.env.NFT_IMAGE,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
